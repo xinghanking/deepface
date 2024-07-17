e35ef7c44759d455eddfcd7186c898a7816ac879
刘重量 <39993639+xinghanking@users.noreply.github.com>
2024-07-17 23:03:43 +0800

    init

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
+import numpy
 import uvicorn
 from deepface import DeepFace
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(client_max_size=1024 * 1024 * 10)
 
 
 class Item(BaseModel):
     avatar_path: str
+    avatar_data: str = None
 
 
 @app.post('/analyze')
 async def analyze(item: Item):
-    avatar_path = item.avatar_path
-    info = DeepFace.analyze(avatar_path, ('age'))
+    if item.avatar_data is not None and item.avatar_data.strip() != '':
+        image = numpy.frombuffer(item.avatar_data)
+    else:
+        if item.avatar_path is not None and item.avatar_path.strip() != '':
+            image = item.avatar_path
+        else:
+            return {
+                "code": 1
+            }
+
+    info = DeepFace.analyze(image, ('age', 'gender'))
     age = info[0]["age"]
     return {
-        'code': 0,
-        'data': {'age': age}
+        "code": 0,
+        "data": {"age": age}
     }
-
-
-uvicorn.run(app, host='0.0.0.0', port=80)
