f21506a79f5712ab142180d9e605baf12b0c3ffb
刘重量 <39993639+xinghanking@users.noreply.github.com>
2024-07-25 21:05:21 +0800

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,7 @@
 
 BASE_ROOT=$(dirname $(dirname "$(readlink -f "$0")"))
 cd "$BASE_ROOT"
-python -m venv .venv
+python3 -m venv .venv
 source .venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt --no-cache-dir
