efd84322d29a55fb163c3052725a21482dc9825b
刘重量 <39993639+xinghanking@users.noreply.github.com>
2024-07-25 23:15:11 +0800

    update

--- bin/install.sh
+++ bin/install.sh
@@ -6,5 +6,6 @@ python3 -m venv .venv
 source .venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt --no-cache-dir
+mkdir -p log
 chmod +x bin/run.sh
 ./bin/run.sh start
